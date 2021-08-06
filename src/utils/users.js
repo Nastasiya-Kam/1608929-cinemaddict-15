13fcdc9cbba7d3723eca85422a49cbbc4b82fe0e
Анастасия <kamanina963@gmail.com>
2021-08-06 07:32:33 +0300

    Добавлено перечисление для званий пользователя

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,12 +1,18 @@
+const Grades = {
+  LOW: 1,
+  MIDDLE: 10,
+  HIGH: 20,
+};
+
 const getRating = (films) => {
   let rating = '';
   const watched = films.filter((film) => film.isWatched).length;
 
-  if (watched <= 10 && watched >= 1) {
+  if (watched <= Grades.MIDDLE && watched >= Grades.LOW) {
     rating = 'novice';
-  } else if (watched <= 20) {
+  } else if (watched <= Grades.HIGH) {
     rating = 'fan';
-  } else if (watched > 20) {
+  } else if (watched > Grades.HIGH) {
     rating = 'Movie Buff';
   }
 
