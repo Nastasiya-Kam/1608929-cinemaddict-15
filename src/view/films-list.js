6ff8fdf73f336bf95d17db4205a5f9a9532cbb64
Анастасия <kamanina963@gmail.com>
2021-08-08 21:33:09 +0300

    Исправлен баг с отображением заголовков для списков фильмов

--- src/view/films-list.js
+++ src/view/films-list.js
@@ -5,7 +5,7 @@ const createFilmsListTemplate = (isExtraList, title) => {
 
   return (
     `<section class="${classList}">
-      <h2 class="films-list__title visually-hidden">${title}</h2>
+      <h2 class="films-list__title ${(!isExtraList) ? 'visually-hidden' : ''}">${title}</h2>
       <div class="films-list__container">
       </div>
     </section>`
