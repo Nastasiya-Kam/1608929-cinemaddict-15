bd1553dd39f369ff47b3a9033f32cebef53d611d
Анастасия <kamanina963@gmail.com>
2021-08-08 21:29:23 +0300

    Рефакторинг меню-фильтрации на сайте

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,5 +1,36 @@
 const Filter = {watchList: 'watchList', watched: 'watched', favorite: 'favorite'};
 
+const Filters = [
+  {
+    typeFilter: 'all',
+    href: '#all',
+    isActive: true,
+    title: 'All movies',
+    isCount: false,
+  },
+  {
+    typeFilter: Filter.watchList,
+    href: '#watchlist',
+    isActive: false,
+    title: 'Watchlist',
+    isCount: true,
+  },
+  {
+    typeFilter: Filter.watched,
+    href: '#history',
+    isActive: false,
+    title: 'History',
+    isCount: true,
+  },
+  {
+    typeFilter: Filter.favorite,
+    href: '#favorites',
+    isActive: false,
+    title: 'Favorites',
+    isCount: true,
+  },
+];
+
 const filmToFilterMap = {
   [Filter.watchList]: (films) => films.filter((film) => film.isWatchList).length,
   [Filter.watched]: (films) => films.filter((film) => film.isWatched).length,
@@ -13,4 +44,4 @@ const generateFilter = (films) => {
   return filter;
 };
 
-export {generateFilter};
+export {generateFilter, Filters};
--- src/view/site-menu.js
+++ src/view/site-menu.js
@@ -1,15 +1,15 @@
 import {createElement} from '../utils/dom.js';
+import {Filters} from '../utils/filter.js';
 
 const createSiteMenuTemplate = (filter) => {
-  const {watchList, watched, favorite} = filter;
+  let filterTemplate = '';
+
+  for (const element of Filters) {
+    filterTemplate += `<a href="${element.href}" class="main-navigation__item ${(element.isActive) ? 'main-navigation__item--active' : ''}">${element.title}${(element.isCount) ? `<span class="main-navigation__item-count">${filter[element.typeFilter]}</span>` : ''}</a>`;
+  }
 
   return (`<nav class="main-navigation">
-    <div class="main-navigation__items">
-      <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-      <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">${watchList}</span></a>
-      <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">${watched}</span></a>
-      <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">${favorite}</span></a>
-    </div>
+    <div class="main-navigation__items">${filterTemplate}</div>
     <a href="#stats" class="main-navigation__additional">Stats</a>
   </nav>`);
 };
