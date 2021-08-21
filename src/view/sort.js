cdb32b0a8eb6f4a4ac02e249ce067d79b2757f9b
Анастасия <kamanina963@gmail.com>
2021-08-21 21:44:07 +0300

    Добавлены обработчики событий на клики по сортировке

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,16 +1,35 @@
 import AbstractView from './abstract.js';
+import {SortType} from '../const.js';
 
 const createSortTemplate = () => (
   `<ul class="sort">
-    <li><a href="#" class="sort__button">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button sort__button--active">Sort by rating</a></li>
+    <li><a href="#" class="sort__button" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+    <li><a href="#" class="sort__button" data-sort-type="${SortType.DATE}">Sort by date</a></li>
+    <li><a href="#" class="sort__button" data-sort-type="${SortType.RATING}">Sort by rating</a></li>
   </ul>`
-);
+); // sort__button--active
 
 class Sort extends AbstractView {
+  constructor() {
+    super();
+
+    this._onSortTypeChange = this._onSortTypeChange.bind(this);
+  }
+
   getTemplate() {
     return createSortTemplate();
+  }
+
+  _onSortTypeChange(evt) {
+    if (evt.target.tagName === 'A') {
+      evt.preventDefault();
+      this._callback.sortTypeChange(evt.target.dataset.sortType);
+    }
+  }
+
+  setOnSortTypeChange(callback) {
+    this._callback.sortTypeChange = callback;
+    this.getElement().addEventListener('click', this._onSortTypeChange);
   }
 }
 
