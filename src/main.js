b292b29c785667ab00713afaa6cb2c64bba12f55
Анастасия <kamanina963@gmail.com>
2021-08-21 21:46:58 +0300

    Переименован презентер

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import ProfileView from './view/profile.js';
 import SiteMenuView from './view/site-menu.js';
-import SortView from './view/sort.js';
 import StatisticsView from './view/statistics.js';
 import {generateFilm} from './mock/film.js';
 import {generateFilter} from './utils/filter.js';
 import {getRating} from './utils/users.js';
 import {getNumberFilms} from './utils/films.js';
 import {renderElement} from './utils/dom.js';
-import FilmListPresenter from './presenter/filmList.js';
+import FilmBoardPresenter from './presenter/film-board.js';
 
 const FILM_DEVELOPER_COUNT = 22;
 
@@ -24,10 +23,9 @@ const footerStatistics = site.querySelector('.footer__statistics');
 
 renderElement(siteHeader, new ProfileView(rating).getElement());
 renderElement(siteMain, new SiteMenuView(filter).getElement());
-renderElement(siteMain, new SortView().getElement());
 
 renderElement(footerStatistics, new StatisticsView(numberFilms).getElement());
 
-const filmListPresenter = new FilmListPresenter(siteMain);
+const filmBoardPresenter = new FilmBoardPresenter(siteMain);
 
-filmListPresenter.init(films);
+filmBoardPresenter.init(films);
