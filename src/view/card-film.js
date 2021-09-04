3e2994ba082eeba42f8371222f908204d77d9ea1
Анастасия <kamanina963@gmail.com>
2021-09-04 08:24:55 +0300

    Вынесена функция определения длины описания

--- src/view/card-film.js
+++ src/view/card-film.js
@@ -1,27 +1,21 @@
 import {getCardDate} from '../utils/dates.js';
+import {getShortDescription} from '../utils/films.js';
 import AbstractView from './abstract.js';
-
-const MAX_LENGTH_DESCRIPTION = 140;
 
 const createCardFilmTemplate = (data) => {
   const {name, rating, release, duration, genres, img, description, comments, isWatchList, isWatched, isFavorite} = data;
-
-  const descriptionSliced = (description.length > MAX_LENGTH_DESCRIPTION)
-    ? `${description.slice(0, MAX_LENGTH_DESCRIPTION - 1)}...`
-    : description;
-  const date = getCardDate(release);
 
   return (
     `<article class="film-card">
       <h3 class="film-card__title">${name}</h3>
       <p class="film-card__rating">${rating}</p>
       <p class="film-card__info">
-        <span class="film-card__year">${date}</span>
+        <span class="film-card__year">${getCardDate(release)}</span>
         <span class="film-card__duration">${duration}</span>
         <span class="film-card__genre">${genres[0]}</span>
       </p>
       <img src="./images/posters/${img}" alt="" class="film-card__poster">
-      <p class="film-card__description">${descriptionSliced}</p>
+      <p class="film-card__description">${getShortDescription(description)}</p>
       <a class="film-card__comments">${comments.length} comments</a>
       <div class="film-card__controls">
         <button class="film-card__controls-item film-card__controls-item--add-to-watchlist ${(isWatchList) ? 'film-card__controls-item--active' : ''}" type="button">Add to watchlist</button>
